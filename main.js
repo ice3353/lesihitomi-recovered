82d6b3f01f397e913df5e0c06c75c333d8c23391
stk2598 <stk2598>
2022-11-18 16:25:08 +0900

    시계:자바스크립트 분리

--- main.js
+++ main.js
@@ -33,3 +33,21 @@ function verify() { //verify 함수 선언, 이 함수는 사이트가 시작될
 	request.open("GET", 'https://api.ipify.org?format=json'); //XMlHttpRequest GET으로 https://api.ipify.org?format=json 의 내용(접속자의 ip주소)를 가져온다
 	request.send(null); //위의 open 요청을 보낸다
 }
+var Target = document.getElementById("clock");
+var Target_apm = document.getElementById("apm");
+function clock() {
+    var time = new Date();
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+    var AmPm ="AM";
+    if(hours > 12){   
+        var AmPm ="PM";
+        hours %= 12;
+    }
+    Target.innerText = 
+    `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    Target_apm.innerText = `${AmPm}`;
+}
+clock();
+setInterval(clock, 1000); // 1초마다 실행
