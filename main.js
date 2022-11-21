12ee1b84df2b28076a8abd77ee2f1595f1f6284d
stk2598 <stk2598>
2022-11-21 16:23:04 +0900

    시계 잠시제거

--- main.js
+++ main.js
@@ -47,9 +47,9 @@ function clock() {
         var AmPm ="PM";
         hours %= 12;
     }
-    Target.innerText = 
+    Target.innerHTML = 
     `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-    Target_apm.innerText = `${AmPm}`;
+    Target_apm.innerHTML = `${AmPm}`;
 }
 clock();
 setInterval(clock, 1000); // 1초마다 실행
